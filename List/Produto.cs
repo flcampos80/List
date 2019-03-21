ee656d7c34d1c682f5bb9b1ae78a8dcd7d0c6b09
flcampos80 <flcampos80@gmail.com>
2019-03-21 12:28:30 -0300

    primeiro commit

---
+++ List/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Produto
+    {
+        private int codigo;
+        private string descricao;
+        private int quantidade;
+        private double preco;
+
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+        public int Quantidade
+        {
+            get { return quantidade; }
+            set { quantidade = value; }
+        }
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+
+        public Produto(int c, string d, int q, double p)
+        {
+            this.codigo = c;
+            this.descricao = d;
+            this.quantidade = q;
+            this.preco = p;
+        }
+
+        public Produto()
+            : this(0, "", 0, 0.0)
+        { }
+
+        public double custo()
+        {
+            return quantidade * preco;
+        }
+    }
+}
---
+++ List/Produtos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Produtos
+    {
+        List<Produto> listaProdutos = new List<Produto>();
+
+        internal List<Produto> ListaProdutos
+        {
+            get { return listaProdutos; }
+            set { listaProdutos = value; }
+        }
+
+        public void adicionar(Produto p)
+        {
+            listaProdutos.Add(p);
+        }
+        public bool remover(Produto p)
+        {
+            return (listaProdutos.Remove(p));
+        }
+        public Produto pesquisar(Produto p)
+        {
+            Produto resultado = new Produto();
+            foreach (Produto prod in this.ListaProdutos)
+            {
+                if (prod.Equals(p))
+                    resultado = p;
+            }
+
+            return resultado;
+        }
+
+        public double custoTotal()
+        {
+            double cTotal = 0.0;
+            foreach (Produto p in ListaProdutos)
+            {
+                cTotal += p.custo();
+            }
+
+            return cTotal;
+        }
+    }
+}
